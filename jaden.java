c0b6d2f21ab037746c25944836ad81710778ede2
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2022-08-09 14:10:47 +0900

    Create jaden.java
    채점 결과
    정확성: 47.2
    효율성: 0.0
    합계: 47.2 / 50
    테스트 케이스 하나를 통과시키지 못했다. 다른 사람들의 풀이를 보며 좀더 공부해야겠다

---
+++ jaden.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+/**
+ * test
+ */
+public class jaden {
+
+    public static void main(String[] args) {
+        solution("3people  unFollowed me");
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+
+       
+        String[] strarr = s.split(" ");
+
+        for(int i=0;i<strarr.length;i++){
+            String str = strarr[i];
+            if(str.isEmpty()){
+                answer+=str+" ";
+            }else{
+                str = str.toLowerCase();
+          
+            
+               if(str.length() == 1){
+                answer+= str.toUpperCase()+" ";
+                   
+               }else{
+               str  = str.substring(0, 1).toUpperCase()+str.substring(1);
+               answer+=str+" ";
+               }
+
+               
+            }
+        }
+
+
+        return answer.trim();
+    }
+}
