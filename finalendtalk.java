60715a037ffd44250a9e6229a86c0bb81bc5c255
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2022-08-23 14:11:52 +0900

    Create finalendtalk.java
    endtalk.java로 채점했을 때 3개의 테스트 케이스가 통과하지 못했다. 다른 사람들의 코드를 보면서 문제가 요구하는 것을 정확하게 파악하게 됐다. 그리고 아주 간단하게 코드로 구현할 수 있다는 것을 알았다.

---
+++ finalendtalk.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+
+public class finalendtalk {
+
+
+
+    public static void main(String[] args) {
+        int[] result = solution(2, new String[]{"hello", "one", "even", "never", "now", "world", "draw"}) ;
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] solution(int n, String[] words) {
+        int[] answer = new int[]{0,0};
+
+        ArrayList<String>list = new ArrayList<>();
+       
+        for(int i=0;i<words.length;i++){
+
+
+            if(i>0){
+            if(words[i-1].toCharArray()[words[i-1].length()-1] != words[i].toCharArray()[0]){
+               // System.out.println(i);
+                return new int[]{(i%n)+1,(i/n)+1};
+            }
+            }
+
+            if(list.contains(words[i]))return new int[]{(i%n)+1,(i/n)+1};
+
+            list.add(words[i]);
+        }
+        
+
+        return answer;
+    }
+}
