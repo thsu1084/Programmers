dc62dbd71df52212f569da149838c4a4ab5d79b1
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2022-11-07 12:04:26 +0900

    Create integertriangle.java

---
+++ integertriangle.java
@@ -0,0 +1,25 @@
+class integertriangle {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        for(int i=1;i<triangle.length;i++){
+            for(int j=0;j<triangle[i].length;j++){
+                
+                if(i == j){
+                     triangle[i][j]= triangle[i][j] + triangle[i-1][j-1];
+                }else if(j == 0){
+                     triangle[i][j] = triangle[i][j] + triangle[i-1][j];
+                }else{
+                
+                int left = triangle[i][j] + triangle[i-1][j-1];
+                int right = triangle[i][j] + triangle[i-1][j];
+                triangle[i][j] = Math.max(left,right);
+                }
+                
+                answer = Math.max(answer,triangle[i][j]);
+            }
+        }
+        
+        return answer;
+    }
+}
